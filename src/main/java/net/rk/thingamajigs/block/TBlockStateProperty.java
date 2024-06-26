8c35a908de40223ec6ae404609cea56ab2a4757c
blueyMasterMods <roberttkahelin8@users.noreply.github.com>
2024-06-26 18:13:10 -0400

    blockpropertycustom
    add new block property class

---
+++ src/main/java/net/rk/thingamajigs/block/TBlockStateProperty.java
@@ -0,0 +1,8 @@
+package net.rk.thingamajigs.block;
+
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+
+public class TBlockStateProperty {
+    public static final BooleanProperty ON = BooleanProperty.create("on");
+    public static final BooleanProperty CONNECTED = BooleanProperty.create("connected");
+}
